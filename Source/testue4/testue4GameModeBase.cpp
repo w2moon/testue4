6c3e57e689d3ae348cc97f46ac6f2521ee72a4b6
w2moon <w2moon@gmail.com>
2016-12-26 10:44:35 +0800

    Initial commit

---
+++ Source/testue4/testue4GameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "testue4.h"
+#include "testue4GameModeBase.h"
+
+
+
+
---
+++ Source/testue4/testue4GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "testue4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTUE4_API Atestue4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
